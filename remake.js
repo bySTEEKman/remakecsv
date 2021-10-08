5a7cc6b725b76c166de239e1f768466c72069d54
Yan <bysteekman@gmail.com>
2021-10-08 18:22:04 +0300

    refactor format of date and added it to key

--- remake.js
+++ remake.js
@@ -1,18 +1,34 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const moment = require('moment');
 const results = [];
+const answ = [];
 
 fs.createReadStream('old.csv')
-    .pipe(csv(['Name', 'Date', 'WorkTime']))
+    .pipe(csv(['Name', 'Date', 'Time']))
     .on('data', (data) => results.push(data))
     .on('end', () => {
+        function sortByName(a, b){
+            return a.Name.toLowerCase() > b.Name.toLowerCase() ? 1 : -1;
+        }
+        results.sort(sortByName);
+        function makeobj(res){
+            let obj = {};
+            let formateDate = moment(`${res.Date}`).format('YYYY-MM-DD');
+            obj.name = `${res.Name}`
+            obj[formateDate] =  `${res.Time}`;
+            answ.push(obj);
+        }
+        results.forEach(makeobj);
         const csvWriter = createCsvWriter({
             path: 'done.csv',
             header: [
-                {id: 'Name/Date', title: 'Name/Date'}
+                {id: 'name', title: 'Name/Date'}
             ]
         });
+
+
         csvWriter.writeRecords(answ)       // returns a promise
             .then(() => {
                 console.log('...Done');
